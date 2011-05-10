ea4acf895a94bca1cb15688fad10efe4efe6a8a6
Inilus ZIO <inilus@gmail.com>
2011-05-10 10:57:22 +0400

    Init

--- reporter.rb
+++ reporter.rb
@@ -5,127 +5,283 @@
 # Docs for Yaml: 				http://santoro.tk/mirror/ruby-core/classes/YAML.html
 # Docs for ProgressBar: http://0xcc.net/ruby-progressbar/index.html.en
 # Docs for TinyTds: 		https://github.com/rails-sqlserver/tiny_tds#readme
-# Docs for Spreadsheet:	http://spreadsheet.rubyforge.org/GUIDE_txt.html
+# Docs for HAML:				http://haml-lang.com/docs/yardoc/Haml.html
+# Docs for RubyZip: 		http://rubyzip.sourceforge.net/classes/Zip/ZipFile.html
 
 require 'yaml'
 require 'progressbar'
 require 'tiny_tds'
-require 'spreadsheet'
+require 'haml'
+require 'zip/zip'
 
 class Reporter
 
-	def initialize ( name_project, department, period=Hash[ :start => Time.now, :duration_in_month => 1 ] )
+	def initialize
 		config = YAML.load_file( "config.yml" )
 		@client = TinyTds::Client.new( :username => config[:db][:username], :password => config[:db][:password], :dataserver => config[:db][:dataserver], :database => config[:db][:database] )
-		@project = Hash[ 
-			:name 			=> name_project, 
-			:department => department,
-			:period 		=> period 
-		]	
-		
-#		@book_report = Spreadsheet::Workbook.new
-#		@sheet_report = @book_report.create_worksheet( :name => 'Report' )	
-	end
-
-	def load_data
-		project = @client.execute( createSqlQuery( :select_project ) ).each( :symbolize_keys => true )[0]
-		if project.nil?
-			puts "Don't find project \"#{ @project[:name] }\"!"
-			exit( 1 )
-		end
-		@project[:id_wbs] = project[:wbs_id]
-		@project[:id_project] = project[:proj_id]
-		@project[:name_wbs] = project[:wbs_name]
-		
-		@actv_types = @client.execute( createSqlQuery( :select_actvtype ) ).each( :symbolize_keys => true )
-		@actv_codes = @client.execute( createSqlQuery( :select_actvcode ) ).each( :symbolize_keys => true )
-		@actv_task_actvs = @client.execute( createSqlQuery( :select_actvcode ) ).each( :symbolize_keys => true )
-		@tasks = @client.execute( createSqlQuery( :select_task ) ).each( :symbolize_keys => true )
 	end
 	
-	def read_form_report
-		book = Spreadsheet.open( 'forms/plan.xls' )
-		sheet = book.worksheet( 0 )
-		
-		## Save footer and clear
-		footer = sheet.row( 11 )	
-		sheet.update_row( 11, '' )
+	def get_all_projects
+		return @client.execute( createSqlQuery( :select_projects ) ).each( :symbolize_keys => true )
+	end
 
-		## Insert new data
-		sheet[1, 5] = sheet.row( 1 )[5] + @project[:department].to_s
+	def load_data( name_project, period=Hash[ :start => "2011-04-01", :finish => "2011-06-01" ] )		
+		@project = Hash[ :period 		=> period ]
+		
+		project = nil
+	
+		if name_project.class == String	
+			@project[:name]	= name_project
+			project = @client.execute( createSqlQuery( :select_project ) ).each( :symbolize_keys => true )
+			if project.nil?
+				puts "Don't find project \"#{ @project[:name] }\"!"
+				exit( 1 )
+			else
+				project = project[0]
+				@project[:id_project] = project[:proj_id]
+			end			
+		else
+			@project[:id_project] = name_project
+			project = @client.execute( createSqlQuery( :select_project_by_id ) ).each( :symbolize_keys => true )[0]
+			@project[:name]	= project[:wbs_short_name]
+		end
+		@project[:id_wbs] = project[:wbs_id]		
+		@project[:name_wbs] = project[:wbs_name]
+		
+	end
+	
+	def work_with_department
+		departments = @client.execute( createSqlQuery( :select_actvtype, { :name => "Route" }  ) ).each( :symbolize_keys => true )
+		## ProgressBar
+	  pbar = ProgressBar.new( "Create report", departments.size )	  
+	  dir = nil
+		departments.each do |department|			
+			if department[:PARENT_actv_code_id].nil?				
+				dir = create_report( department[:short_name] ) if dir.nil?
+				dir << create_report( department[:short_name] )[1]
+			end
+			## ProgressBar
+			pbar.inc
+		end
+		## ProgressBar
+		pbar.finish
+		return dir
+	end
+	
+	def create_report( department="" )
+    haml_engine = Haml::Engine.new( File.read( 'html_report_tmp.haml' ) )		
+		
 		months = Array["январь", "февраль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"]
-		sheet[2, 5] = sheet.row( 2 )[5] + " #{ months[@project[:period][:start].month - 1] } месяц #{ @project[:period][:start].year } г."
 		
-		format = Spreadsheet::Format.new 	:weight => 700,
-																			:name => "Arial Narrow",
-																			:weight => 700,
-																			:bottom_color => :border, 
-																			:top_color => :border, 
-																			:left_color => :border, 
-																			:right_color => :border, 
-																			:diagonal_color => :border, 
-																			:pattern_fg_color => :border,
-																			:horizontal_align => :center, 
-																			:vertical_align		=> :bottom,
-																			:left							=> true, 
-																			:bottom						=> true
-                                   		
-#		14.times do |i| 
-			sheet.row( 10 ).set_format( 0, sheet.row( 9 ).format( 0 ) )
-			sheet[10, 0] = "10"
-			sheet[10, 1] = "10"
-			sheet.row( 11 ).set_format( 1, sheet.row( 9 ).format( 1 ) )
-			sheet[11, 0] = "11"
-			sheet[11, 1] = "11"
-			sheet.row( 12 ).default_format = format
-			sheet.row( 13 ).default_format = format
-#		end
+		data = {
+		  :department => department,
+		  :period     => "на #{ months[@project[:period][:start][5, 2].to_i - 1] } месяц #{ @project[:period][:start][0,4] } г.",
+      :heads      => [
+        [
+          "№ п/п",
+          "№ чертежа",
+          "Наименование",
+          "Маршрут/ Номер точки маршрута",
+          "№ опер. по ТП",
+          [ "Даты", 4 ],
+          [ "Трудоемкость, н/ч", 2 ],
+          [ "Количество, шт.", 3 ],
+          "Подтверждающий документ",
+          "Примеч."   
+        ],
+        [ "План. старт",
+          "План. финиш",
+          "Факт. старт",
+          "Факт. финиш",
+          
+          "План. отработ.",
+          "Факт. отработ.",
+          
+          "Всего",
+          "План",
+          "Факт"
+        ]   
+      ]
+    }
+    
+    data[:tasks] = prepare_data( department )
+
+    ## Save file
+    dir = "public/output/#{ @project[:name] }"
+    file = ""
+    unless data[:tasks].empty?    	
+    	if not File::directory?( dir )
+    		Dir.mkdir( dir )
+    	end	
+    	file = "#{ @project[:period][:start] }-#{ @project[:period][:finish] } #{ department }"
+    	html = haml_engine.render(nil, data)
+		  File.open( "#{ dir }/#{ file }.html", "w" ) do |file_report|
+				file_report.puts html
+				file_report.close
+			end
+			Zip::ZipFile.open("#{ dir }/#{ file[0, 21] }.zip", Zip::ZipFile::CREATE) { |zipfile|
+				zipfile.get_output_stream("#{ file }.html") { |f| f.puts html }
+			}
+			
+		end
+		result = Array[ dir[7..-1] ]
+		result << file unless file.empty?
+		return result
+	end
+	
+	def prepare_data( department="" )
+		result = Array.new
+				
+		departments = @client.execute( createSqlQuery( :select_actvtype, { :name => "Route", :value => department }  ) ).each( :symbolize_keys => true )
 		
-		## Restore footer
-		14.times do |i| 
-			sheet[15, i] = footer[i]
-			sheet.row( 15 ).set_format( i, footer.format( i ) )
+		id_structure_last = Hash[ :id => -1, :i => 0 ]
+		departments.each_with_index do |department, index_dep|
+			tasks = @client.execute( createSqlQuery( :select_tasks_with_structure, { :id_actv_code => department[:actv_code_id], :name_actv_code => "Structure" }  ) ).each( :symbolize_keys => true )
+			if not tasks.empty?					
+				index_task = 0
+				tasks.each do |task|
+					if id_structure_last[:id] != task[:id_structure]
+						id_structure_last[:id] = task[:id_structure] 
+						id_structure_last[:i] = id_structure_last[:i] + 1 
+						index_task = 0
+						result << [
+												"<b>#{ id_structure_last[:i] }</b>",
+												"",
+												"<b>Этап #{ task[:short_name_structure] }. #{ task[:name_structure] }</b>"
+											]
+					end
+					
+					index_task += 1
+					
+					result << [ 
+											"#{ id_structure_last[:i] }.#{ index_task }.",
+											udf_code_to_value( @client.execute( createSqlQuery( :select_actvcode, { :id_task => task[:task_id], :name_actvtype => "DO" } ) ).each( :symbolize_keys => true ) ),
+											task[:task_name],
+											udf_code_to_value( @client.execute( createSqlQuery( :select_actvcode, { :id_task => task[:task_id], :name_actvtype => "Route full" } ) ).each( :symbolize_keys => true ) ) + " / " + udf_code_to_value( route_num_str = @client.execute( createSqlQuery( :select_actvcode, { :id_task => task[:task_id], :name_actvtype => "Step route" } ) ).each( :symbolize_keys => true ) ),
+											"",
+											task[:target_start_date].strftime( "%d.%m.%Y" ),
+											task[:target_end_date].strftime( "%d.%m.%Y" ),
+											"",
+											"",
+											udf_code_to_value( @client.execute( createSqlQuery( :select_udf_code, { :id_task => task[:task_id], :name => "Трудоёмкость по ТП" } ) ).each( :symbolize_keys => true ), :number ),			
+											"",
+											udf_code_to_value( @client.execute( createSqlQuery( :select_udf_code, { :id_task => task[:task_id], :name => "Количество" } ) ).each( :symbolize_keys => true ), :number )
+									  ] 
+				end	
+			
+			end
 		end
 		
-		## Save file
-		book.write 'output/report.xls'
+		return result
 	end
 
-
-	def createSqlQuery( name_query, params={} )
-	  case name_query
-	  
-#	  	when
-#	  		## Required: params{  }
-#	  		return 
-	  		
-			## def load_data
-		  when :select_project
-		  	## Required: params{  }
-		  	return "SELECT TOP 1 [wbs_id], [proj_id], [wbs_name] FROM [dbo].[PROJWBS] WHERE [wbs_short_name] LIKE '#{ @project[:name].to_s }' AND [proj_node_flag] = 'Y'; "
-		  when :select_actvtype
-		  	## Required: params{  }
-		  	return "SELECT [actv_code_type_id], [actv_code_type] FROM [dbo].[ACTVTYPE]; "
-		  when :select_actvcode
-	  		## Required: params{  }
-	  		return "SELECT ACTVCODE.[actv_code_id], ACTVCODE.[actv_code_type_id], ACTVCODE.[short_name], ACTVCODE.[actv_code_name], ACTVCODE_PARENT.[actv_code_id] as PARENT_actv_code_id, ACTVCODE_PARENT.[short_name] as short_name, ACTVCODE_PARENT.[actv_code_name] as PARENT_actv_code_name 
-FROM [dbo].[ACTVCODE] AS ACTVCODE 
-LEFT JOIN [dbo].[ACTVCODE] AS ACTVCODE_PARENT ON ACTVCODE_PARENT.[actv_code_id] = ACTVCODE.[parent_actv_code_id]; "
-		  when :select_taskactv
-	  		## Required: params{  }
-	  		return "SELECT [task_id],[actv_code_type_id],[actv_code_id],[proj_id] FROM [dbo].[TASKACTV] WHERE [proj_id] = #{ @project[:id_project].to_s }; "
-			when :select_task
-	  		## Required: params{  }
-	  		start  = "#{ @project[:period][:start].year }-#{ @project[:period][:start].month }-01"
-	  		finish = "#{ @project[:period][:start].year }-#{ @project[:period][:start].month + @project[:period][:duration_in_month] }-01"
-	  		return "SELECT TASK.[status_code], TASK.[task_name], TASK.[remain_drtn_hr_cnt], TASK.[target_drtn_hr_cnt], TASK.[target_start_date], TASK.[target_end_date] 
-FROM [dbo].[TASK] AS TASK
---LEFT JOIN 
-
-WHERE TASK.[proj_id]	= #{ @project[:id_project].to_s } AND TASK.[wbs_id] = #{ @project[:id_wbs].to_s } AND TASK.[task_type] = 'TT_Task'	AND ( ( TASK.[target_start_date] BETWEEN CONVERT(DATETIME, '#{ start }', 102) AND CONVERT(DATETIME, '#{ finish }', 102) ) OR ( TASK.[target_end_date] BETWEEN CONVERT(DATETIME, '#{ start }', 102) AND CONVERT(DATETIME, '#{ finish }', 102) ) ); "
-	  		# TODO Add Left join and filter by department
-		  else
-		  	return nil
+	private 
+	
+		def udf_code_to_value( array, type=:string )
+			case type
+				when :string
+					return ( not array.empty? ) ? array[0][:short_name].to_s : ""
+				when :number
+					return ( not array.empty? ) ? array[0][:udf_number].to_i.to_s : 0.to_s
+			end
 		end
-	end
+		
+		def createSqlQuery( name_query, params={} )
+			case name_query
+			
+	#	  	when
+	#	  		## Required: params{  }
+	#	  		return 
+					
+				## def prepare_data
+				when :select_actvtype
+					## Required: params{ :name [, :value ] }
+					sql = Array.new
+					if not params[:value].nil?
+						sql[0] = "AND ( ACTVCODE.short_name = '#{ params[:value] }' OR ACTVCODE_PARENT.short_name = '#{ params[:value] }' )"
+					end
+					return "SELECT ACTVCODE.[actv_code_id], ACTVCODE.[short_name] , ACTVCODE_PARENT.[actv_code_id] as PARENT_actv_code_id 
+FROM [dbo].[ACTVTYPE] AS ACTVTYPE
+	LEFT JOIN [dbo].[ACTVCODE] AS ACTVCODE
+ 		ON ACTVCODE.[actv_code_type_id] = ACTVTYPE.[actv_code_type_id]
+	LEFT JOIN [dbo].[ACTVCODE] AS ACTVCODE_PARENT 
+		ON ACTVCODE_PARENT.[actv_code_id] = ACTVCODE.[parent_actv_code_id]		
+WHERE ACTVTYPE.[actv_code_type] LIKE '#{ params[:name] }' #{ sql[0] }; "
+				when :select_tasks_with_structure
+					## Required: params{ :id_actv_code, :name_actv_code }
+					return "SELECT 
+	TASK.[task_id], TASK.[task_name], 
+	--TASK.[status_code], 
+	TASK.[remain_drtn_hr_cnt], TASK.[target_drtn_hr_cnt], 
+	TASK.[target_start_date], TASK.[target_end_date],
+	ACTVCODE_structure.actv_code_id AS id_structure, ACTVCODE_structure.actv_code_name AS name_structure , ACTVCODE_structure.short_name AS short_name_structure
+FROM [dbo].[TASKACTV] AS TASKACTV
+	LEFT JOIN [dbo].[TASK] AS TASK
+		ON TASK.[task_id] = TASKACTV.[task_id] AND TASK.[proj_id] = #{ @project[:id_project] } 
+			AND TASK.[task_type] = 'TT_Task'
+			AND TASK.[delete_date] is null
+	LEFT JOIN [dbo].[TASKACTV] AS TASKACTV_structure
+		ON TASKACTV_structure.task_id = TASK.task_id
+	LEFT JOIN [dbo].[ACTVCODE] AS ACTVCODE_structure
+		ON ACTVCODE_structure.actv_code_id = TASKACTV_structure.actv_code_id
+WHERE TASKACTV.[actv_code_id] = #{ params[:id_actv_code] } 
+	 AND TASKACTV.[proj_id] = #{ @project[:id_project] } 
+	 AND TASKACTV_structure.actv_code_type_id = (SELECT TOP 1 ACTVTYPE_structure.actv_code_type_id 
+		FROM [dbo].[ACTVTYPE] AS ACTVTYPE_structure 
+		WHERE ACTVTYPE_structure.actv_code_type LIKE '#{ params[:name_actv_code] }')
+			AND TASK.[act_end_date] is NULL 
+			AND ( 
+				( TASK.[target_start_date] BETWEEN CONVERT(DATETIME, '#{ @project[:period][:start] }', 102 ) 
+					AND CONVERT( DATETIME, '#{ @project[:period][:finish] }', 102 ) )
+				OR ( TASK.[act_start_date] BETWEEN CONVERT(DATETIME, '#{ @project[:period][:start] }', 102 ) 
+					AND CONVERT( DATETIME, '#{ @project[:period][:finish] }', 102 ) )
+				OR ( ( TASK.[target_end_date] BETWEEN CONVERT(DATETIME, '#{ @project[:period][:start] }', 102 ) 
+					AND CONVERT( DATETIME, '#{ @project[:period][:finish] }', 102 ) ) )
+				OR ( ( TASK.[target_start_date] < CONVERT(DATETIME, '#{ @project[:period][:finish] }', 102) )
+					AND ( TASK.[target_end_date] > CONVERT(DATETIME, '#{ @project[:period][:start] }', 102) ) ) )
+ORDER BY ACTVCODE_structure.actv_code_id, TASK.[task_id]"
+				when :select_actvcode
+					## Required: params{ :id_task, :name_actvtype }
+					return "SELECT TOP 1 ACTVCODE.[short_name], ACTVCODE.[actv_code_name]
+FROM [dbo].[TASKACTV] AS TASKACTV
+	LEFT JOIN [dbo].[ACTVCODE] AS ACTVCODE
+		ON ACTVCODE.actv_code_id = TASKACTV.actv_code_id
+WHERE TASKACTV.[task_id] = #{ params[:id_task] }
+	AND TASKACTV.[actv_code_type_id] = ( 
+		SELECT TOP 1 ACTVTYPE.[actv_code_type_id] 
+		FROM [dbo].[ACTVTYPE] AS ACTVTYPE
+		WHERE ACTVTYPE.[actv_code_type] LIKE '#{ params[:name_actvtype] }' )"
+				when :select_udf_code
+					## Required: params{ :id_task, :name }
+					return "SELECT TOP 1 UDFVALUE.[udf_number]
+FROM [dbo].[UDFTYPE] AS UDFTYPE
+	LEFT JOIN [dbo].[UDFVALUE] AS UDFVALUE
+		ON UDFVALUE.[udf_type_id] = UDFTYPE.[udf_type_id]
+			AND UDFVALUE.[fk_id] = #{ params[:id_task] }
+WHERE UDFTYPE.[table_name] = 'TASK' AND UDFTYPE.[udf_type_label] LIKE '#{ params[:name] }'"
+										
+				## def load_data
+				when :select_project
+					## Required: params{  }
+					return "SELECT TOP 1 PROJWBS.[wbs_id], PROJWBS.[proj_id], PROJWBS.[wbs_name] 
+FROM [dbo].[PROJWBS] AS PROJWBS
+WHERE PROJWBS.[wbs_short_name] = '#{ @project[:name].to_s }' AND [proj_node_flag] = 'Y'; "
+					when :select_project_by_id
+					## Required: params{  }
+					return "SELECT TOP 1 PROJWBS.[wbs_id], PROJWBS.[wbs_name], PROJWBS.[wbs_short_name]
+FROM [dbo].[PROJWBS] AS PROJWBS
+WHERE PROJWBS.[proj_id] = #{ @project[:id_project].to_s } AND [proj_node_flag] = 'Y'; "
+
+				## def get_all_projects
+				when :select_projects
+					## Required: params{  }
+					return "SELECT PROJWBS.[wbs_id], PROJWBS.[proj_id], PROJWBS.[wbs_name], PROJWBS.[wbs_short_name] 
+FROM [dbo].[PROJWBS] AS PROJWBS
+WHERE PROJWBS.[proj_node_flag] = 'Y' AND PROJWBS.[parent_wbs_id] != null
+ORDER BY PROJWBS.[wbs_name] ASC; "
+
+				else
+					return nil
+			end
+		end
 end
--- runner.rb
+++ runner.rb
@@ -6,15 +6,16 @@ require_relative  'reporter'
 
 class Runner
 	def initialize
-		if ARGV.empty? or ARGV[0].nil? or ARGV[1].nil?
-			puts "Incorrect parameter! For example: \"ruby runner.rb '91.2446' 4\""
+		if ARGV.empty? or ARGV[0].nil? or ARGV[1].nil? or ARGV[2].nil?
+			puts "Incorrect parameter! For example: \"ruby runner.rb '91.2446' '2011-04-25' '2011-06-30'\""
   		exit( 1 )
 		end
 		
-		reporter = Reporter.new( ARGV[0].to_s, ARGV[1].to_s )
+		reporter = Reporter.new
 		
-		reporter.load_data
-		reporter.read_form_report
+		reporter.load_data( ARGV[0].to_s, Hash[ :start => ARGV[1].to_s, :finish => ARGV[2].to_s ] )
+		reporter.work_with_department
+#		reporter.create_report
 		
 	end
 	
